c7940454d51475c20d447491a9c10a188f305748
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-21 00:23:35 +0700

    Add

--- app/app/screens/about.js
+++ app/app/screens/about.js
@@ -1,12 +1,14 @@
 import VnNativeJs from 'vn-native-js';
+import VnNativeCore from 'vn-native-js/VnNativeCore';
+import componentObject from 'vn-native-js/componentObject';
 import header from '../components/header.js';
 import general from '../css/general.js';
 import running from '../components/running.js';
 let aboutScreen = new Object;
 aboutScreen.render = function(){
-        let aboutBody = document.createElement('div');
-        aboutBody.appendChild(header('About'));
-        aboutBody.appendChild(running());
+        let aboutBody = new componentObject;
+        aboutBody.childComponent(header('About'));
+        aboutBody.childComponent(running());
         return aboutBody;
 }
 export default aboutScreen;
