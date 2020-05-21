96e9d589fa2fe395aac9c3220069184b5a7d8c2b
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-21 19:39:55 +0700

    Update source example

--- app/hooks/run.js
+++ app/hooks/run.js
@@ -1,11 +1,9 @@
 let fs = require('fs');
 let webpackConfig = require('../webpack.config');
 var cmd = require('node-cmd');
-cmd.get('cordova serve',function(err,data) {
-    if(err) {
-        console.error("Hidden server can not start at port :8000");
-    }
-});
+/**
+ * Serve 
+ */
 fs.readFile("./config.xml",(err,data) => {
     if (err) {
         return console.log(err);
